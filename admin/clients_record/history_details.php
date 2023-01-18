86fbcbdb95becfad49d34f089069a0cca2e5e45c
gabriel-andrei <gaboabarintos30@gmail.com>
2023-01-18 14:16:38 +0800

    Update history_details.php

--- admin/clients_record/history_details.php
+++ admin/clients_record/history_details.php
@@ -81,101 +81,112 @@ if(isset($_GET['id'])){
                     <div class="col-9 py-1 px-2 border mb-0"><?= isset($user_name) ? $user_name : '' ?></div>
                 </div>
                 <div class="clear-fix mb-2"></div>
-                <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <fieldset class="px-2 py-1 border">
-                        <legend class="w-auto px-3">Services</legend>
-                        <div class="clear-fix mb-2"></div>
-                        <table class="table table-striped table-bordered" id="service-list">
-                            <colgroup>
-                                <col width="30%">
-                                <col width="30%">
-                                <col width="20%">
-                                <col width="20%">
-                            </colgroup>
-                            <thead>
-                                <tr class="bg-light-blue text-center">
-                                    <th class="text-center">Service</th>
-                                    <th class="text-center">Details</th>
-                                    <th class="text-center">Cylinder</th>
-                                    <th class="text-center">Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                $service_amount = 0;
-                                $ts_qry = $conn->query("SELECT ts.*, s.service as `service` , s.service_sub, s.cylinder FROM `transaction_services` ts inner join `service_list` s on ts.service_id = s.id where ts.`transaction_id` = '{$id}' ");
-                                while($row = $ts_qry->fetch_assoc()):
-                                    $service_amount += $row['price'];
-                                ?>
-                                <tr>
-                                    <td class="text-center"><?= $row['service'] ?></td>
-                                    <td class="text-center"><?= $row['service_sub'] ?></td>
-                                    <td class="text-center"><?= $row['cylinder'] ?></td>
-                                    <td class="text-center service_price"><?= format_num($row['price']) ?></td>
-                                </tr>
-                                <?php endwhile; ?>
-                            </tbody>
-                            <tfoot>
-                                <tr class="bg-gradient-secondary">
-                                    <th colspan="3" class="text-right">Total</th>
-                                    <th class="text-center" id="service_total"><?= isset($service_amount) ? format_num($service_amount): 0 ?></th>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </fieldset>
-                </div>
+                <?php 
+                    $ts_qry = $conn->query("SELECT ts.*, s.service as `service` , s.service_sub, s.cylinder FROM `transaction_services` ts inner join `service_list` s on ts.service_id = s.id where ts.`transaction_id` = '{$id}' ");
+                    $count = mysqli_num_rows($ts_qry);
+                    ?>
+                <div class="row <?= $count==0? 'hidden':''?>">                
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <fieldset class="px-2 py-1 border">
+                            <legend class="w-auto px-3">Services</legend>
+                            <div class="clear-fix mb-2"></div>
+                            <table class="table table-striped table-bordered" id="service-list">
+                                <colgroup>
+                                    <col width="30%">
+                                    <col width="30%">
+                                    <col width="20%">
+                                    <col width="20%">
+                                </colgroup>
+                                <thead>
+                                    <tr class="bg-light-blue text-center">
+                                        <th class="text-center">Service</th>
+                                        <th class="text-center">Details</th>
+                                        <th class="text-center">Cylinder</th>
+                                        <th class="text-center">Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                    $service_amount = 0;
+                                    $ts_qry = $conn->query("SELECT ts.*, s.service as `service` , s.service_sub, s.cylinder FROM `transaction_services` ts inner join `service_list` s on ts.service_id = s.id where ts.`transaction_id` = '{$id}' ");
+                                    while($row = $ts_qry->fetch_assoc()):
+                                        $service_amount += $row['price'];
+                                    ?>
+                                    <tr>
+                                        <td class="text-center"><?= $row['service'] ?></td>
+                                        <td class="text-center"><?= $row['service_sub'] ?></td>
+                                        <td class="text-center"><?= $row['cylinder'] ?></td>
+                                        <td class="text-center service_price"><?= format_num($row['price']) ?></td>
+                                    </tr>
+                                    <?php endwhile; ?>
+                                </tbody>
+                                <tfoot>
+                                    <tr class="bg-gradient-secondary">
+                                        <th colspan="3" class="text-right">Total</th>
+                                        <th class="text-center" id="service_total"><?= isset($service_amount) ? format_num($service_amount): 0 ?></th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </fieldset>
+                    </div>
                 </div>
                 
-                <div class="row">                        
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <fieldset class="px-2 py-1 border">
-                    <legend class="w-auto px-3">Products</legend>
-                        <div class="clear-fix mb-2"></div>
-                        <table class="table table-striped table-bordered" id="product-list">
-                            <colgroup>
-                                <col width="15%">
-                                <col width="35%">
-                                <col width="15%">
-                                <col width="20%">
-                                <col width="20%">
-                            </colgroup>
-                            <thead>
-                                <tr class="bg-light-blue">
-                                    <th class="text-center">Engine Model</th>
-                                    <th class="text-center">Item Name</th>
-                                    <th class="text-center">Qty</th>
-                                    <th class="text-center">Price</th>
-                                    <th class="text-center">Total</th>
-                                </tr>
-                            </thead>
-                        
-                            <tbody>
-                            <?php 
-                                $product_total = 0;
-                                $tp_qry = $conn->query("SELECT tp.*, p.name as `product`, p.engine_model FROM `transaction_products` tp inner join `product_list` p on tp.product_id = p.id where tp.`transaction_id` = '{$id}' ");
-                                while($row = $tp_qry->fetch_assoc()):
-                                    $product_total += ($row['price'] * $row['qty']);
-                            ?>
-                                <tr>
-                                    <td class="text-center"><?= $row['engine_model'] ?></td>
-                                    <td class="text-center"><?= $row['product'] ?></td>
-                                    <td class="text-center"><?= $row['qty'] ?></td>
-                                    <td class="text-center product_price"><?= $row['price'] ?></td>
-                                    <td class="text-center product_total"><?= format_num($row['price'] * $row['qty']) ?></td>
-                                </tr>
-                            <?php endwhile; ?>
-                            </tbody>
-                        
-                            <tfoot>
-                                <tr class="bg-gradient-secondary">
-                                    <th colspan="4" class="text-right">Total</th>
-                                    <th class="text-center" id="product_total"><?= isset($product_total) ? format_num($product_total): 0 ?></th>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </fieldset>
-                </div>
+                <?php 
+                    $tp_qry = $conn->query("SELECT tp.*, p.name as `product` , p.engine_model
+                        FROM `transaction_products` tp 
+                        inner join `product_list` p on tp.product_id = p.id 
+                        where tp.`transaction_id` = '{$id}' ");
+                    $count = mysqli_num_rows($tp_qry);
+                    ?>
+                <div class="row <?= $count==0? 'hidden':''?>">                 
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <fieldset class="px-2 py-1 border">
+                        <legend class="w-auto px-3">Products</legend>
+                            <div class="clear-fix mb-2"></div>
+                            <table class="table table-striped table-bordered" id="product-list">
+                                <colgroup>
+                                    <col width="15%">
+                                    <col width="35%">
+                                    <col width="15%">
+                                    <col width="20%">
+                                    <col width="20%">
+                                </colgroup>
+                                <thead>
+                                    <tr class="bg-light-blue">
+                                        <th class="text-center">Engine Model</th>
+                                        <th class="text-center">Item Name</th>
+                                        <th class="text-center">Qty</th>
+                                        <th class="text-center">Price</th>
+                                        <th class="text-center">Total</th>
+                                    </tr>
+                                </thead>
+                            
+                                <tbody>
+                                <?php 
+                                    $product_total = 0;
+                                    $tp_qry = $conn->query("SELECT tp.*, p.name as `product`, p.engine_model FROM `transaction_products` tp inner join `product_list` p on tp.product_id = p.id where tp.`transaction_id` = '{$id}' ");
+                                    while($row = $tp_qry->fetch_assoc()):
+                                        $product_total += ($row['price'] * $row['qty']);
+                                ?>
+                                    <tr>
+                                        <td class="text-center"><?= $row['engine_model'] ?></td>
+                                        <td class="text-center"><?= $row['product'] ?></td>
+                                        <td class="text-center"><?= $row['qty'] ?></td>
+                                        <td class="text-center product_price"><?= $row['price'] ?></td>
+                                        <td class="text-center product_total"><?= format_num($row['price'] * $row['qty']) ?></td>
+                                    </tr>
+                                <?php endwhile; ?>
+                                </tbody>
+                            
+                                <tfoot>
+                                    <tr class="bg-gradient-secondary">
+                                        <th colspan="4" class="text-right">Total</th>
+                                        <th class="text-center" id="product_total"><?= isset($product_total) ? format_num($product_total): 0 ?></th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </fieldset>
+                    </div>
                 </div>
                 <hr>
                 <div class="clear-fix mb-3"></div>
